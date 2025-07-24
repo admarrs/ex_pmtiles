66c60ea85e25fb85600d4507c7d2e939ebe8a650
Alan Marrs <alan.marrs@srt-marine.com>
2025-07-24 12:26:22 +0100

    chore: missing deps.get

--- mix.exs
+++ mix.exs
@@ -9,10 +9,12 @@ defmodule ExPmtiles.MixProject do
       app: :ex_pmtiles,
       version: "0.1.0",
       elixir: "~> 1.17",
-      start_permanent: Mix.env() == :prod,
-      deps: deps(),
+      name: "ExPmtiles",
+      source_url: @source_url,
       package: package(),
-      docs: docs()
+      deps: deps(),
+      docs: docs(),
+      start_permanent: Mix.env() == :prod
     ]
   end
 
